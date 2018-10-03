c1bc277831eb38da4ea2e23ffaf719c3ed181b8a
Vivek Gupta <Vivek.Gupta@easyjet.com>
2018-10-03 20:48:38 +0100

    first commit

--- src/main/java/com/easyjet/test/spark/mllib/Application.java
+++ src/main/java/com/easyjet/test/spark/mllib/Application.java
@@ -4,14 +4,21 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Logger.getLogger("org").setLevel(Level.OFF);//does not work
 		SpringApplication.run(Application.class, args);
 	}
+	
+	
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
 }
