a3ae44657738ccd3ea5e56f6cce3a63aa7548b63
Vivek Gupta <Vivek.Gupta@easyjet.com>
2018-10-04 12:43:38 +0100

    first commit

--- src/main/java/com/easyjet/test/spark/mllib/Application.java
+++ src/main/java/com/easyjet/test/spark/mllib/Application.java
@@ -2,24 +2,44 @@ package com.easyjet.test.spark.mllib;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Vivek Main Called");
+		System.out.println("Main Called");
 		SpringApplication.run(Application.class, args);
 	}
 	
 	
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		System.out.println("Vivek configure Called");
+    	System.out.println("configure Called");
         return application.sources(Application.class);
     }
 
+}
+@RestController
+class HelloController {
+	@Autowired
+	TestSparkMLLib testSparkMLLib;
+	
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+    	
+    	System.out.println("Vivek Here");
+    	String[] args = new String[2];
+    	testSparkMLLib.startKafka(args);
+    	
+        return "Hi " + name + " !";
+ 
+    }
 }
