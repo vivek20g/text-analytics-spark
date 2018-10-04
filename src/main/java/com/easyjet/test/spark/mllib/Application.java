96cf1feb54382b698bdf945aa0f50a504ffa75a4
Vivek Gupta <Vivek.Gupta@easyjet.com>
2018-10-04 14:18:53 +0100

    Initial commit

--- src/main/java/com/easyjet/test/spark/mllib/Application.java
+++ src/main/java/com/easyjet/test/spark/mllib/Application.java
@@ -1,18 +1,19 @@
 package com.easyjet.test.spark.mllib;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.apache.log4j.Level;
+//import org.apache.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+//public class Application extends SpringBootServletInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -21,18 +22,21 @@ public class Application extends SpringBootServletInitializer {
 	}
 	
 	
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    /*protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     	System.out.println("configure Called");
         return application.sources(Application.class);
-    }
+    }*/
 
 }
+/*
 @RestController
+
 class HelloController {
 	@Autowired
 	TestSparkMLLib testSparkMLLib;
 	
     @RequestMapping("/hello/{name}")
+    
     String hello(@PathVariable String name) {
     	
     	System.out.println("Vivek Here");
@@ -42,4 +46,4 @@ class HelloController {
         return "Hi " + name + " !";
  
     }
-}
+}*/
